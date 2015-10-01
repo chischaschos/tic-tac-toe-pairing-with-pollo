f1023b74c14ada3a1424877902e741ab0f3ebc72
Pollo&Chischaschos Pairing FTW! <rodacato@gmail.com, larin.s931@gmail.com>
2015-10-01 13:38:38 -0500

    Refactor game to track movements and a state depending on the current quantity movements

--- spec/core_game_spec.rb
+++ spec/core_game_spec.rb
@@ -6,36 +6,113 @@ end
 
 class CoreGame
 
-  attr_reader :movements
+  attr_reader :board
 
   def initialize
     @players = [ Player.new, Player.new ]
-    @movements = []
+    @board = [Array.new(3, ''), Array.new(3, ''), Array.new(3, '')]
   end
 
   def move(x, y)
-    @movements << [x,y]
+    symbol = @board.flatten.select{|item| item != ''}.length.odd? ? 'O' : 'X'
+    @board[x][y] = symbol
   end
 
+  def state
+    return 'unstarted' if @board.flatten.select{|item| item != ''}.count == 0
+    return 'on-progress' if @board.flatten.select{|item| item != ''}.count > 0
+    return 'finished'
+  end
 end
 
 describe CoreGame do
   subject(:game) { CoreGame.new }
 
-  it 'allows players to make movements' do
-    game.move(0, 0)
+  let(:x_winning_movements) do
+    [
+       [0, 0],
+       [0, 1],
+       [1, 1],
+       [1, 2],
+     ]
+  end
 
-    expect(game.state).to eq 'on-progress'
+  it 'plays a full tic tac toe game' do
+    expect(game.state).to eq 'unstarted'
+
+    x_winning_movements.each do |coords|
+      game.move(*coords)
+      expect(game.state).to eq 'on-progress'
+    end
+
     expect(game.board).to eq ([
-      ['X', '', ''],
-      ['', '', ''],
+      ['X', 'O', ''],
+      ['', 'X', 'O'],
       ['', '', ''],
     ])
 
-    game.move(0, 1)
+    game.move(2, 2)
+    expect(game.state).to eq 'finished'
+    expect(game.winner).to eq 'X'
+  end
 
-    expect(game.movements).to have(2).items
-    expect(game.movements.last).to eq([0, 1])
+  describe '#board' do
+    it 'tracks the movement on 0, 0' do
+      game.move(0, 0)
+      expect(game.board).to eq ([
+        ['X', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ])
+    end
+
+    it 'tracks the movement on 0, 1' do
+      game.move(0, 1)
+      expect(game.board).to eq ([
+        ['', 'X', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ])
+    end
+
+    it "the first movement can't be an O" do
+      game.move(0, 1)
+      expect(game.board).to_not eq ([
+        ['', 'O', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ])
+    end
+
+    it "odd movements are tracked as O" do
+      game.move(0, 1)
+      game.move(0, 2)
+      expect(game.board).to eq ([
+        ['', 'X', 'O'],
+        ['', '', ''],
+        ['', '', ''],
+      ])
+    end
+  end
+
+  describe '#state' do
+    it "has a different state when the game hasn't started" do
+      expect(game.state).to eq 'unstarted'
+    end
+
+    it 'changes the state fo the board after a movement' do
+      game.move(1, 1)
+      expect(game.state).to eq 'on-progress'
+    end
+
+    it 'determines when the game is finished' do
+      x_winning_movements.each do |coords|
+        game.move(*coords)
+      end
+
+      game.move(2, 2)
+      expect(game.state).to eq 'finished'
+    end
   end
 
   xit 'should not allow a player to move twice'
