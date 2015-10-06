0340c7b9d0beae4e45629fb0ff7a9c472e190a33
Pollo&Chischaschos Pairing FTW! <rodacato@gmail.com, larin.s931@gmail.com>
2015-10-05 19:30:06 -0500

    - Unstub finished? - Implement finished? method - Refactor finished? method

--- spec/core_game_spec.rb
+++ spec/core_game_spec.rb
@@ -23,10 +23,38 @@ class CoreGame
   end
 
   def finished?
+    combinations = diagonal_combinations + row_combinations
+    combinations.find { |row| similar_in_row?(row) }
   end
 
   def winner
     'X'
+  end
+
+  private
+
+  def similar_in_row?(row)
+    symbols = %w( O X )
+    row.uniq.length == 1 && symbols.include?(row.first)
+  end
+
+  def diagonal_combinations
+    [
+      [@board[0][0], @board[1][1], @board[2][2]],
+      [@board[2][0], @board[1][1], @board[0][2]]
+    ]
+  end
+
+  def row_combinations
+    combinations = []
+
+    [@board, @board.transpose].each do |tmp_board|
+      2.downto(0) do |i|
+        combinations << tmp_board[i]
+      end
+    end
+
+    combinations
   end
 end
 
@@ -57,7 +85,6 @@ describe CoreGame do
     ])
 
     game.move(2, 2)
-    allow(game).to receive(:finished?) { true }
     expect(game.state).to eq 'finished'
     expect(game.winner).to eq 'X'
   end
@@ -118,15 +145,90 @@ describe CoreGame do
 
       game.move(2, 2)
 
-      allow(game).to receive(:finished?) { true }
       expect(game.state).to eq 'finished'
     end
   end
 
   describe '#finished?' do
-    it 'had a player completing a row with same symbols'
-    it 'had a player completing a column with same symbols'
-    it 'had a player completing a diagonal with same symbols'
+    context 'when winning by completing a row' do
+      let(:movements) do
+        [
+          [0, 0],
+          [1, 0],
+          [0, 1],
+          [1, 1],
+          [0, 2]
+        ]
+      end
+
+      it 'has a finished state' do
+        movements.each do |coords|
+          game.move(*coords)
+        end
+
+        expect(game).to be_finished
+      end
+    end
+
+    context 'when winning by completing a column' do
+      let(:movements) do
+        [
+          [0, 0],
+          [0, 1],
+          [1, 0],
+          [1, 1],
+          [2, 0]
+        ]
+      end
+
+      it 'has a finished state' do
+        movements.each do |coords|
+          game.move(*coords)
+        end
+
+        expect(game).to be_finished
+      end
+    end
+
+    context 'when winning by completing a diagonal' do
+      let(:movements) do
+        [
+          [0, 0],
+          [0, 1],
+          [1, 1],
+          [1, 2],
+          [2, 2]
+        ]
+      end
+
+      it 'has a finished state' do
+        movements.each do |coords|
+          game.move(*coords)
+        end
+
+        expect(game).to be_finished
+      end
+    end
+  end
+
+  context 'when winning by completing an inverse diagonal' do
+    let(:movements) do
+      [
+        [0, 2],
+        [0, 1],
+        [1, 1],
+        [1, 2],
+        [2, 0]
+      ]
+    end
+
+    it 'has a finished state' do
+      movements.each do |coords|
+        game.move(*coords)
+      end
+
+      expect(game).to be_finished
+    end
   end
 
   xit 'should not allow a player to move twice'
