86d840d898437fab2d51bdea1ec9f777718cabdf
Pollo&Chischaschos Pairing FTW! <rodacato@gmail.com, larin.s931@gmail.com>
2015-10-26 13:19:31 -0600

    Introduce router and first controller

--- lib/game_api.rb
+++ lib/game_api.rb
@@ -1,6 +1,26 @@
-# This to become our GameApi application
-class GameApi
-  def call(_env)
-    [200, {}, 'guadap']
+require 'json'
+require 'rack/response'
+
+module GameApi
+  # This to become our GameApi application
+  class Router
+    def call(env)
+      request = Rack::Request.new(env)
+
+      app = case request.path
+            when %r{\/games\/create} then GamesController.new
+            end
+
+      app.call(env)
+    end
+  end
+
+  # GameApi::Create handles creation of games
+  class GamesController
+    def call(_env)
+      response = Rack::Response.new
+      response.write({ id: 1 }.to_json)
+      response.finish
+    end
   end
 end
--- spec/feature/play_game_via_http_spec.rb
+++ spec/feature/play_game_via_http_spec.rb
@@ -3,25 +3,47 @@ require 'rack'
 require_relative '../../lib/game_api'
 
 describe 'Play game via http' do
-  let(:server_url) { 'localhost:9292' }
+  let(:server_url) { 'localhost:8080' }
 
   before do
     @pid = fork do
       Rack::Server.start(
-        app: GameApi.new,
-        daemonize: true,
+        app: GameApi::Router.new,
         server: 'webrick'
       )
     end
+    sleep 1
   end
 
   after do
     `kill -9 #{@pid}`
   end
 
-  it 'a user plays a game' do
-    # response = RestClient.post(server_url + '/games/create', {})
+  describe 'ping tests' do
+    it 'creates games'
 
+    it 'creates movements'
+
+    it 'has a root page'
+
+    it '500 on unexisting pages'
+  end
+
+  it 'a user plays a game' do
+    # Start the game
+    response = RestClient.post(server_url + '/games/create', {})
+    new_game_response = JSON.parse(response)
+    expect(new_game_response['id']).to be_an(Integer)
+
+    # game_id = new_game_response['id']
+    # url = server_url + "/games/#{game_id}/moves"
+    # response = RestClient.post(url, {x: 1, y:1})
     # new_game_response = JSON.parse(response)
+    # board = [
+    # ['', '', ''],
+    # ['', 'X', ''],
+    # ['', '', '']
+    # ]
+    # expect(new_game_response[:board]).to eq(board)
   end
 end
